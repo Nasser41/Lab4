56c4a7ab81b03e0a1de95d0a3d95f251617f4ab3
Nasser41 <Nasser.mnr@gmail.com>
2024-02-25 21:04:03 +0300

    Upload html files

--- TaxCalculator/views.py
+++ TaxCalculator/views.py
@@ -4,16 +4,14 @@ from django.http import HttpResponse
 tax_rate = 0.15
 
 def Defaultpath(request):
-    return render(request,'TaxCalculator/DefaultPath.html', {'message': 'This is a site to calculate tax.'})
+    return render(request,'TaxCalculator/DefaultPath.html')
 
 def anyNumber(request, price):
-    try:
-        price = float(price)
-        total_price = price * (1 + tax_rate)
-        context = {'price': price, 'total_price': total_price}
-        return render(request, 'TaxCalculator/anyNumber.html', context)
-    except ValueError:
-        return render(request, 'TaxCalculator/error.html', {'message': 'Invalid price entered.'})
+    price = float(price)
+    total_price = price * (1 + tax_rate)
+    context = {'price': price, 'total_price': total_price}
+    return render(request, 'TaxCalculator/anyNumber.html', context)
+   
 
 def taxrate(request):
     context = {'tax_rate': tax_rate}
